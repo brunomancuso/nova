7658b33322c8586f97ed7d4d0f33da80b573aace
brunomancuso <bruno.mancuso@gmail.com>
2026-08-11 19:30:39 -0300

    Update index.js

--- js/model/index.js
+++ js/model/index.js
@@ -3,4 +3,3 @@ export { Ball }       from './Ball.js';
 export { Step }       from './Step.js';
 export { Drill }      from './Drill.js';
 export { DrillStore } from './DrillStore.js';
-export { clamp }      from './helpers.js';
